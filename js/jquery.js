22be3f8e5a319f1e554fbf8d12ee19d737c3986a
velvetcerise <velvet.cerise@hotmail.com>
2018-02-02 23:50:32 +0000

    flags and views

--- js/jquery.js
+++ js/jquery.js
@@ -5,3 +5,22 @@ $(function(){
 
 
 });
+
+//Flags
+$("#demo").intlTelInput();
+
+//Valid input with the number
+function validarSiNumero(numero){
+  if (!/^([0-9])*$/.tesst(numero))
+    alert("El valor " + numero + " no es un número");
+}
+
+
+// getting LAB code
+function getRandomCode() {
+  return Math.floor((Math.random()));
+}
+
+$('#aleatory').click( function() {
+    alert('your code is: LAB' + 'getRandomCode');
+});
